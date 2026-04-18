36779033be1717f8030dddcc540450b943fe166d
InoAbary <140265797+InoAbary@users.noreply.github.com>
2026-04-18 20:18:22 +0800

    initial commit

---
+++ Schema/crowsource_db.sql
@@ -0,0 +1,19 @@
+CREATE SCHEMA IF NOT EXISTS AH_Crowdsource_DB;
+USE AH_Crowdsource_DB;
+
+CREATE TABLE IF NOT EXISTS Student(
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    birthday DATETIME
+
+);
+
+CREATE TABLE IF NOT EXISTS Annotation(
+	annotate_id INT AUTO_INCREMENT PRIMARY KEY,
+    rule VARCHAR(255),
+    positive_count INT,
+    negative_count INT
+
+);
