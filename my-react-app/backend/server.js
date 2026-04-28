6dfce4a0560bcd0d0a70a3f3aa84c185a1266539
rainexee <157507549+rainexee@users.noreply.github.com>
2026-04-28 12:15:32 +0800

    server.js

--- my-react-app/backend/server.js
+++ my-react-app/backend/server.js
@@ -37,7 +37,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie:{
+    cookie: {
         secure: false,
         httpOnly: true,
         maxAge: 1000 * 60 * 15
@@ -50,8 +50,39 @@ const transporter = nodemailer.createTransport({
         user: process.env.NODE_MAIL,
         pass: process.env.NODE_PASS
     }
-}); 
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, ''));
 });
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again after 15 minutes",
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use('/api/', limiter);
+
+
+app.get('/api/dashboard/', (req, res) => {
+
+});
+
+app.post('/api/upload', (req, res) => {
+    const { upload } = req.body;
+
+    if (!upload || typeof upload !== 'string') {
+        return res.status(400).json({ error: 'Rule must be a valid input!' });
+    }
+
+    //security testing for filesize
+    if (upload.length > 255) {
+        return res.status(400).json({ error: 'Rule cannot exceed 255 characters' });
+    }
+
+    // fix later on
+    db.query('INSERT INTO DATASET ? VALUES ? ? ? ? ?');
+})
